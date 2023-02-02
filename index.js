286d8c842c003890e0a1e90c1f75fb1f13fdc1ab
biplob kumar <biplobkumar125@gmail.com>
2023-02-02 09:24:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+//  celcius to faran hight csale start program 
+
+var cels= parseFloat(prompt(" Enter celcius number :"));
+
+var farn= (cels* 9/5)+32;
+console.log("faranhight = " + farn);
+
+
+//  celcius to faran hight csale  end  program 
+
+
+
+//  faranhight to celsuis  program  start
+
+var farn= parseFloat(prompt(" Enter farnhight number :"));
+var cels=(farn-32) *(5/9);
+console.log("celcius =" +farn);
+
+//  faranhight to celsuis  program  end 
